92bcebb5a297fe47f4388f6cd301bd7c031422f2
joshrod <joshrodriguez17@gmail.com>
2022-04-20 17:27:52 -0500

    Added large desktop animations to Home and About sections. Added curtainjs functionality to McNay image. Manipulated index.html and animations.scss accordingly to add animations functionality

---
+++ src/js/main.js
@@ -0,0 +1,286 @@
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+// /* GLOBAL VARS */
+// let vw = window.innerWidth;
+// let vh = window.innerHeight;
+let vw = document.body.clientWidth;
+let vh = window.innerHeight;
+
+/* CIRCLE EXPANSION ANIMATION */
+
+// Set The Canvas
+const circleCanvas = document.getElementById('black-circle-canvas');
+circleCanvas.width = vw;
+circleCanvas.height = vh;
+
+const circleContext = circleCanvas.getContext('2d');
+
+// Get Starter Circle Parameters
+let starterRadius = (vw < 1200 ? 35 : 50);
+let circleX = calcFirstCircleX(vw);
+let circleY = calcFirstCircleY(vw, vh);
+
+// Make Second Circle Representable as an Animatable Object
+let firstCircle = {
+    radius: starterRadius,
+    x: circleX,
+    y: circleY,
+    globalAlpha: 0
+}
+
+// Get X and Y Circle Coordinates through Functions
+function calcFirstCircleX(viewWidth) {
+    let fromRight;
+
+    switch (true) {
+        case (viewWidth >= 1440):
+            fromRight = 2.083333333333333;
+            break;
+        case (viewWidth >= 1200 && viewWidth < 1440):
+            fromRight = 2.34375;
+            break;
+        default:
+            fromRight = 0;
+    }
+
+    switch (true) {
+        case (viewWidth >= 768 && viewWidth < 1200):
+            return viewWidth - 30 - starterRadius;
+        case (viewWidth < 768):
+            return viewWidth - 15 - starterRadius;
+        default:
+            return ((viewWidth * (100 - fromRight)) / 100) - starterRadius;
+    }
+}
+
+function calcFirstCircleY(windowWidth, windowHeight) {
+    let fromBottom;
+
+    switch (true) {
+        case (windowWidth >= 1200):
+            fromBottom = 4.1666666;
+            break;
+        case (windowWidth >= 992 && windowWidth < 1200):
+            fromBottom = 7.32064421;
+            break;
+        case (windowWidth >= 768 && windowWidth < 992):
+            fromBottom = 9.765625;
+            break;
+        default:
+            fromBottom = 5.556;
+    }
+
+    return ((windowHeight * (100 - fromBottom)) / 100) - starterRadius;
+}
+
+// Draw First Circle
+function drawFirstCircle() {
+    circleContext.clearRect(0, 0, vw, vh);
+    circleContext.beginPath();
+    circleContext.arc(firstCircle.x, firstCircle.y, firstCircle.radius, 0, 2 * Math.PI, false);
+    circleContext.globalAlpha = firstCircle.globalAlpha;
+    circleContext.fillStyle = "#F4F5F5";
+    circleContext.fill();
+}
+
+
+// Get New Circle Parameters
+let deltaX = circleX < vw / 2 ? vw - circleX : circleX;
+let deltaY = circleY < vh / 2 ? vh - circleY : circleY;
+let radius2 = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+// Make Second Circle Representable as an Animatable Object
+let secondCircle = {
+    radius: starterRadius,
+    x: circleX,
+    y: circleY
+}
+
+// Create Timeline to Play Forward and Backward Easier
+const circleTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".hero",
+        start: 'top top',
+        scrub: 0.3,
+    }
+});
+
+// Timeline Animation (include onUpdate function here)
+circleTl.to(secondCircle, {
+    radius: radius2,
+    onUpdate: drawSecondCircle
+});
+circleTl.to('.hero-arrow-container',
+    {
+        rotation: 180
+    },
+    "<"
+);
+
+function drawSecondCircle() {
+
+    circleContext.clearRect(0, 0, vw, vh);
+    circleContext.beginPath();
+    circleContext.arc(secondCircle.x, secondCircle.y, secondCircle.radius, 0, 2 * Math.PI, false);
+    circleContext.fillStyle = "#F4F5F5";
+    circleContext.fill();
+
+}
+
+/* HERO SECTION ANIMATIONS */
+
+// Text Animation, Fade-in Logo, Fade-in Canvas ONLY when first arriving on site
+if (scrollY === 0) {
+    let heroTl = gsap.timeline(
+        {
+            onStart: disableScroll,
+            onComplete: enableScroll
+        }
+    );
+
+    gsap.set('.hero-logo-container', {
+        autoAlpha: 0
+    });
+    gsap.set('.hero-arrow-container', {
+        autoAlpha: 0
+    });
+
+    heroTl.from('.split-text', {
+        y: 200,
+        stagger: 0.03,
+        duration: 0.6,
+        ease: "circ.out"
+    });
+    heroTl.to('.hero-logo-container', {
+        autoAlpha: 1
+    }, "-=0.5");
+    heroTl.fromTo(firstCircle,
+        {
+            globalAlpha: 0
+        },
+        {
+            globalAlpha: 1,
+            onUpdate: drawFirstCircle
+        }
+    );
+    heroTl.to('.hero-arrow-container',
+        {
+            autoAlpha: 1
+        },
+        "<"
+    );
+}
+
+// Scroll to Top with GSAP on Arrow Click
+const heroArrow = document.querySelector('.hero-arrow-container');
+
+heroArrow.addEventListener('click', function () {
+    gsap.to(window,
+        {
+            scrollTo: 0,
+            ease: 'power2'
+        }
+    )
+});
+
+
+/* ABOUT ME SECTION ANIMATIONS */
+
+// Stagger Reveal About Me Paragraph Text
+
+const aboutTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".about-me",
+        start: "top center",
+        // toggleActions: 'play none none reverse',
+        // onEnter: () => {
+        //     aboutTl.timeScale(1.0);
+        // },
+        // onEnterBack: () => {
+        //     aboutTl.timeScale(3.5);
+        // },
+        // markers: true
+    }
+})
+
+aboutTl.fromTo('.inside-word-split',
+    {
+        y: 200,
+    },
+    {
+        y: 0,
+        stagger: 0.03,
+        duration: 0.5,
+        ease: "circ.out",
+    }
+);
+
+/* MARQUEE ANIMATIONS */
+
+// Marquees Go In and Out on Scroll
+gsap.utils.toArray('.marquee').forEach((marquee, index) => {
+    const wrapper = marquee.querySelector('.marquee-wrapper');
+    const [x, xEnd] = (index % 2) ? ['100%', (wrapper.scrollWidth - marquee.offsetWidth) * -1] : [wrapper.scrollWidth * -1, 0];
+    gsap.fromTo(wrapper, { x }, {
+        x: xEnd,
+        scrollTrigger: {
+            trigger: ".marquee-divider",
+            scrub: 0.2
+        }
+    });
+});
+
+/* GLOBAL WINDOW RESIZE */
+
+window.addEventListener('resize', windowResize);
+
+function windowResize() {
+    vw = window.innerWidth;
+    vh = window.innerHeight;
+}
+
+/* GLOBAL FUNCTIONS */
+
+/* PREVENT SCROLLING */
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
---
+++ src/js/webgl.js
@@ -0,0 +1,232 @@
+// // wait for everything to be ready
+// window.addEventListener("load", () => {
+//     // set up our WebGL context and append the canvas to our wrapper
+//     const curtains = new Curtains({
+//         container: "mcnay-canvas",
+//         watchScroll: false
+//     });
+//     // get our plane element
+//     const planeElement = document.getElementsByClassName("mcnay-plane")[0];
+//     // set our initial parameters (basic uniforms)
+//     const params = {
+//         vertexShaderID: "plane-vs", // our vertex shader ID
+//         fragmentShaderID: "plane-fs", // our fragment shader ID
+//         uniforms: {
+//             time: {
+//                 name: "uTime", // uniform name that will be passed to our shaders
+//                 type: "1f", // this means our uniform is a float
+//                 value: 0,
+//             },
+//         },
+//     };
+//     // create our plane using our curtains object, the HTML element and the parameters
+//     const plane = new Plane(curtains, planeElement, params);
+//     plane.onRender(() => {
+//         // use the onRender method of our plane fired at each requestAnimationFrame call
+//         plane.uniforms.time.value++; // update our time uniform value
+//     });
+// });
+
+window.addEventListener("load", () => {
+    // track the mouse positions to send it to the shaders
+    const mousePosition = new Vec2();
+    // we will keep track of the last position in order to calculate the movement strength/delta
+    const mouseLastPosition = new Vec2();
+
+    const deltas = {
+        max: 0,
+        applied: 0,
+    };
+
+    // set up our WebGL context and append the canvas to our wrapper
+    const curtains = new Curtains({
+        container: "mcnay-canvas",
+        watchScroll: false, // no need to listen for the scroll in this example
+        pixelRatio: Math.min(1.5, window.devicePixelRatio) // limit pixel ratio for performance
+    });
+
+    // handling errors
+    curtains.onError(() => {
+        // we will add a class to the document body to display original images
+        document.body.classList.add("no-curtains");
+    }).onContextLost(() => {
+        // on context lost, try to restore the context
+        curtains.restoreContext();
+    });
+
+    // get our plane element
+    const planeElements = document.getElementsByClassName("curtains-plane");
+
+
+    const vs = `
+        precision mediump float;
+        // default mandatory variables
+        attribute vec3 aVertexPosition;
+        attribute vec2 aTextureCoord;
+        uniform mat4 uMVMatrix;
+        uniform mat4 uPMatrix;
+        
+        // our texture matrix uniform
+        uniform mat4 simplePlaneTextureMatrix;
+        // custom variables
+        varying vec3 vVertexPosition;
+        varying vec2 vTextureCoord;
+        uniform float uTime;
+        uniform vec2 uResolution;
+        uniform vec2 uMousePosition;
+        uniform float uMouseMoveStrength;
+        void main() {
+            vec3 vertexPosition = aVertexPosition;
+            // get the distance between our vertex and the mouse position
+            float distanceFromMouse = distance(uMousePosition, vec2(vertexPosition.x, vertexPosition.y));
+            // calculate our wave effect
+            float waveSinusoid = cos(5.0 * (distanceFromMouse - (uTime / 75.0)));
+            // attenuate the effect based on mouse distance
+            float distanceStrength = (0.4 / (distanceFromMouse + 0.4));
+            // calculate our distortion effect
+            float distortionEffect = distanceStrength * waveSinusoid * uMouseMoveStrength;
+            // apply it to our vertex position
+            vertexPosition.z +=  distortionEffect / 30.0;
+            vertexPosition.x +=  (distortionEffect / 30.0 * (uResolution.x / uResolution.y) * (uMousePosition.x - vertexPosition.x));
+            vertexPosition.y +=  distortionEffect / 30.0 * (uMousePosition.y - vertexPosition.y);
+            gl_Position = uPMatrix * uMVMatrix * vec4(vertexPosition, 1.0);
+            // varyings
+            vTextureCoord = (simplePlaneTextureMatrix * vec4(aTextureCoord, 0.0, 1.0)).xy;
+            vVertexPosition = vertexPosition;
+        }
+    `;
+
+    const fs = `
+        precision mediump float;
+        varying vec3 vVertexPosition;
+        varying vec2 vTextureCoord;
+        uniform sampler2D simplePlaneTexture;
+        void main() {
+            // apply our texture
+            vec4 finalColor = texture2D(simplePlaneTexture, vTextureCoord);
+            // fake shadows based on vertex position along Z axis
+            finalColor.rgb -= clamp(-vVertexPosition.z, 0.0, 1.0);
+            // fake lights based on vertex position along Z axis
+            finalColor.rgb += clamp(vVertexPosition.z, 0.0, 1.0);
+            // handling premultiplied alpha (useful if we were using a png with transparency)
+            finalColor = vec4(finalColor.rgb * finalColor.a, finalColor.a);
+            gl_FragColor = finalColor;
+        }
+    `;
+
+    // some basic parameters
+    const params = {
+        vertexShader: vs,
+        fragmentShader: fs,
+        widthSegments: 20,
+        heightSegments: 20,
+        uniforms: {
+            resolution: { // resolution of our plane
+                name: "uResolution",
+                type: "2f", // notice this is an length 2 array of floats
+                value: [planeElements[0].clientWidth, planeElements[0].clientHeight],
+            },
+            time: { // time uniform that will be updated at each draw call
+                name: "uTime",
+                type: "1f",
+                value: 0,
+            },
+            mousePosition: { // our mouse position
+                name: "uMousePosition",
+                type: "2f", // again an array of floats
+                value: mousePosition,
+            },
+            mouseMoveStrength: { // the mouse move strength
+                name: "uMouseMoveStrength",
+                type: "1f",
+                value: 0,
+            }
+        }
+    };
+
+    // create our plane
+    // const simplePlane = new Plane(curtains, planeElements[0], params);
+
+    // insert my code here
+    for (let i = 0; i < planeElements.length; i++) {
+        const plane = planeElements[i];
+
+        const simplePlane = new Plane(curtains, plane, params);
+        // if there has been an error during init, simplePlane will be null
+        simplePlane.onReady(() => {
+            // set a fov of 35 to reduce perspective (we could have used the fov init parameter)
+            simplePlane.setPerspective(35);
+
+            // apply a little effect once everything is ready
+            deltas.max = 2;
+
+            // now that our plane is ready we can listen to mouse move event
+            const wrapper = document.getElementsByClassName('project-img-container')[i];
+            console.log(wrapper);
+
+            wrapper.addEventListener("mousemove", (e) => {
+                handleMovement(e, simplePlane);
+            });
+
+            wrapper.addEventListener("touchmove", (e) => {
+                handleMovement(e, simplePlane);
+            }, {
+                passive: true
+            });
+
+        }).onRender(() => {
+            // increment our time uniform
+            simplePlane.uniforms.time.value++;
+
+            // decrease both deltas by damping : if the user doesn't move the mouse, effect will fade away
+            deltas.applied += (deltas.max - deltas.applied) * 0.02;
+            deltas.max += (0 - deltas.max) * 0.01;
+
+            // send the new mouse move strength value
+            simplePlane.uniforms.mouseMoveStrength.value = deltas.applied;
+
+        }).onAfterResize(() => {
+            const planeBoundingRect = simplePlane.getBoundingRect();
+            simplePlane.uniforms.resolution.value = [planeBoundingRect.width, planeBoundingRect.height];
+        }).onError(() => {
+            // we will add a class to the document body to display original images
+            document.body.classList.add("no-curtains");
+        });
+    }
+
+    // handle the mouse move event
+    function handleMovement(e, plane) {
+        // update mouse last pos
+        mouseLastPosition.copy(mousePosition);
+
+        const mouse = new Vec2();
+
+        // touch event
+        if (e.targetTouches) {
+            mouse.set(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
+        }
+        // mouse event
+        else {
+            mouse.set(e.clientX, e.clientY);
+        }
+
+        // lerp the mouse position a bit to smoothen the overall effect
+        mousePosition.set(
+            curtains.lerp(mousePosition.x, mouse.x, 0.3),
+            curtains.lerp(mousePosition.y, mouse.y, 0.3)
+        );
+
+        // convert our mouse/touch position to coordinates relative to the vertices of the plane and update our uniform
+        plane.uniforms.mousePosition.value = plane.mouseToPlaneCoords(mousePosition);
+
+        // calculate the mouse move strength
+        if (mouseLastPosition.x && mouseLastPosition.y) {
+            let delta = Math.sqrt(Math.pow(mousePosition.x - mouseLastPosition.x, 2) + Math.pow(mousePosition.y - mouseLastPosition.y, 2)) / 30;
+            delta = Math.min(4, delta);
+            // update max delta only if it increased
+            if (delta >= deltas.max) {
+                deltas.max = delta;
+            }
+        }
+    }
+});
